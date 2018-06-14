5ce978250aafb83f34d623d87aa053f4a56b06d9
DESKTOP-BBF4VGS\hp <yajizuozuo@163.com>
2018-06-14 09:08:46 +0800

    a

--- js/alert.js
+++ js/alert.js
@@ -1,8 +1,19 @@
 //手机端弹窗样式
-function alert_(title) {
-	$(".alert").remove();
+function alert_(title,callback,fn) {
+	$(".bwui-dialog").remove();
 	var cHtml = "";
-	cHtml = "<div class='alert fade'> " + title + "</div>";
+//	cHtml = "<div class='alert fade'> " + title + "</div>";
+	cHtml += '<div class="bwui-dialog" style="">'
+		cHtml += 	'<div class="bwui-dialog-mask"></div> '
+		cHtml += 	'<div class="bwui-dialog-box">'
+		cHtml += 		'<div class="bwui-dialog-box-title"></div>' 
+		cHtml += 		'<div class="bwui-dialog-box-content">'
+		cHtml += 			'<div> '+ title + '</div>'
+		cHtml += 		'</div> '
+		cHtml += 		'<div class="bwui-dialog-box-footer">'
+		cHtml += 		'<div class="button submit" onclick="'+ callback(fn) +'">知道了</div>'
+		cHtml += 		'</div></div></div>'
+			
 	$("body").append(cHtml);
 }
 
@@ -19,7 +30,16 @@ function reg_phone(num){
 function ajax_url(){
 //	var url = "http://192.168.0.86:8080/adminManage/"
 	var url = "http://hou.ganleyuan.net/"
-
 	return url
 }
 
+function closeTip(closeFix){
+	$('.bwui-dialog').hide()
+	if(closeFix){
+		closeFix()
+	}
+}
+
+function showPop(){
+	$(".com-pop").addClass('show').show()
+}
